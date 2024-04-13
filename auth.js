717d6efb91958f4a9723a3ca9a71df5cd43a9b97
hariom <hariompoddar123@gmail.com>
2024-04-13 11:13:16 +0530

    authentication added

---
+++ auth.js
@@ -0,0 +1,24 @@
+const passport =  require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/User');
+
+passport.use(new LocalStrategy(async(USERNAME, PASSWORD, done) =>{
+    try{
+        // console.log('Received credentials:', USERNAME, PASSWORD);
+        const user = await User.findOne({username: USERNAME});
+        if(!user)
+          return done(null, false, { message: 'Incorrect username.'});
+  
+        const isPasswordMatch = user.password === PASSWORD ? true : false;
+        if(isPasswordMatch){
+          return done(null, user);
+        }else{
+          return done(null, false, {message: 'Incorrect password.'});
+        }
+    }
+    catch(err){
+        return done(err);
+    }
+  }))
+
+  module.exports = passport;
--- server.js
+++ server.js
@@ -3,13 +3,25 @@ const app = express()
 const db = require('./db')
 const bodyParser = require('body-parser');
 require('dotenv').config();
-
+const passport =  require('./auth');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
+// Middleware Function
+const logRequet = (req, res, next) =>{
+  console.log(`[${new Date().toLocaleString()}] Request Made to: ${req.originalUrl}`);
+  next();
+} 
+app.use(logRequet);
+
+
+app.use(passport.initialize());
+
+const localAuthMiddleware = passport.authenticate('local', {session: false})
+
+app.get('/', localAuthMiddleware,function (req, res) {
   res.send('Hello World')
 }) 
 
@@ -17,6 +29,7 @@ app.get('/', function (req, res) {
 
 //   Import the router file
 const personRouter = require('./Router/Person');
+const User = require('./models/User');
 
 // use the routers
 app.use('/user', personRouter);
